7f6999ba6087e2662a73a9181695683fd5d8189a
vladyslav833 <kovalenkovladyslav833@gmail.com>
2023-08-12 04:53:27 -0700

    Commit message

---
+++ challenge.py
@@ -0,0 +1,88 @@
+import math
+
+
+def square_area(side):
+    """Returns the area of a square"""
+    # You have to code here
+    # REMEMBER: Tests first!!!
+    pass
+
+
+def rectangle_area(base, height):
+    """Returns the area of a rectangle"""
+    # You have to code here
+    # REMEMBER: Tests first!!!
+    pass
+
+
+def triangle_area(base, height):
+    """Returns the area of a triangle"""
+    # You have to code here
+    # REMEMBER: Tests first!!!
+    pass
+
+
+def rhombus_area(diagonal_1, diagonal_2):
+    """Returns the area of a rhombus"""
+    # You have to code here
+    # REMEMBER: Tests first!!!
+    pass
+
+
+def trapezoid_area(base_minor, base_major, height):
+    """Returns the area of a trapezoid"""
+    # You have to code here
+    # REMEMBER: Tests first!!!
+    pass
+
+
+def regular_polygon_area(perimeter, apothem):
+    """Returns the area of a regular polygon"""
+    # You have to code here
+    # REMEMBER: Tests first!!!
+    pass
+
+
+def circumference_area(radius):
+    """Returns the area of a circumference"""
+    # You have to code here
+    # REMEMBER: Tests first!!!
+    # Use math.pi for π value
+    pass
+
+
+if __name__ == '__main__':
+    import unittest
+
+    class GeometrySuite(unittest.TestCase):
+
+        def setUp(self):
+            # Initialize the needed values for the tests
+            pass
+
+        def test_square_area(self):
+            # Make this test first...
+
+        def test_rectangle_area(self):
+            # Make this test first...
+
+        def test_triangle_area(self):
+            # Make this test first...
+
+        def test_rhombus_area(self):
+            # Make this test first...
+
+        def test_trapezoid_area(self):
+            # Make this test first...
+
+        def test_regular_polygon_area(self):
+            # Make this test first...
+
+        def test_circumference_area(self):
+            # Make this test first...
+
+        def tearDown(self):
+            # Delete the needed values for the tests
+            pass
+
+    unittest.main()
